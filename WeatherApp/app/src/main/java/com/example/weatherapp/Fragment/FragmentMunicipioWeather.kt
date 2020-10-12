76d9901ebefb0ed8469759f397255f6f240b8968
Daniel Gordillo Rodríguez <danielgordillo9@gmail.com>
2020-10-12 21:05:58 +0200

    AlertDialog

--- WeatherApp/app/src/main/java/com/example/weatherapp/Fragment/FragmentMunicipioWeather.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/Fragment/FragmentMunicipioWeather.kt
@@ -10,22 +10,26 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.Loader
+import com.example.weatherapp.Alert.LoadingDialog
 import com.example.weatherapp.Entities.MunicipioTiempo
 import com.example.weatherapp.R
 import com.example.weatherapp.ViewModel.WeatherInfoFactory
 import com.example.weatherapp.ViewModel.WeatherInfoViewModel
 import kotlinx.android.synthetic.main.municipio_weather_fragment.*
 
-class FragmentMunicipioWeather(var lifeowner: LifecycleOwner, var modelOwner: ViewModelStoreOwner): Fragment(), LoaderManager.LoaderCallbacks<MunicipioTiempo> {
+class FragmentMunicipioWeather(var lifeowner: LifecycleOwner, var modelOwner: ViewModelStoreOwner): Fragment() {
 
+    //, LoaderManager.LoaderCallbacks<MunicipioTiempo>
     var weatherInfoViewModel: WeatherInfoViewModel = WeatherInfoViewModel(lifeowner,modelOwner)
     var weatherInfoFactory: WeatherInfoFactory = WeatherInfoFactory(lifeowner,modelOwner)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+        val loading: LoadingDialog = LoadingDialog(requireActivity())
         val root = inflater.inflate(R.layout.municipio_weather_fragment,container,false)
         weatherInfoViewModel = ViewModelProvider(requireActivity(),weatherInfoFactory).get(WeatherInfoViewModel::class.java)
 
+        loading.startDialog()
         //Observo el municipio con el tiempo para que cuando llegue la informacion ponerla en pantalla
         weatherInfoViewModel.municipioTiempo.observe(lifeowner){ it ->
             TextViewNombreMunicipio.text = it.municipio.NOMBRE
@@ -33,22 +37,25 @@ class FragmentMunicipioWeather(var lifeowner: LifecycleOwner, var modelOwner: Vi
             TextViewHumedad.text = it.humedad
             TextViewTiempo.text = it.stateSky.description
             TextViewTemperaturaManiana.text = it.pronostico.manana.temperatura[0].plus("º aprox")
+            loading.dismissDialog()
         }
 
         return root
     }
 
+
+
     //veo que loader esta bastante depricated pero bueno algo se podra hacer
-    override fun onCreateLoader(id: Int, args: Bundle?): Loader<MunicipioTiempo> {
-        TODO("Not yet implemented")
+    /*override fun onCreateLoader(id: Int, args: Bundle?): Loader<MunicipioTiempo> {
+
     }
 
     override fun onLoadFinished(loader: Loader<MunicipioTiempo>, data: MunicipioTiempo?) {
-        TODO("Not yet implemented")
+
     }
 
     override fun onLoaderReset(loader: Loader<MunicipioTiempo>) {
-        TODO("Not yet implemented")
-    }
+
+    }*/
 
 }
